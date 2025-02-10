d18f28d775d372762722e83898aef89c709f32b3
Praveen Singh <praveen969500@gmail.com>
2025-02-10 22:22:21 +0530

    move about page to support page using use navigate hook.

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,22 @@
-function about() {
+import { useNavigate } from "react-router-dom";
+
+function About() {
+    const navigate = useNavigate();
+    function clickHandler() {
+        //move to support page
+        navigate("/support");
+
+    }
     return (
-        <h1>about page</h1>
+        <div>
+            <h1>about page</h1>
+            <button onClick={clickHandler}>move to support page</button>
+        </div>
+
+
     )
 }
 
 
-export default about;
+
+export default About;
